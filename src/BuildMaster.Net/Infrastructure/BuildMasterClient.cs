9850579b0df397e0582f112d56fe194b3e3ba64c
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-01 07:31:40 +0200

    add infrastructure/servers

--- src/BuildMaster.Net/Infrastructure/BuildMasterClient.cs
+++ src/BuildMaster.Net/Infrastructure/BuildMasterClient.cs
@@ -1,4 +1,8 @@
-﻿using Flurl.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BuildMaster.Net.Infrastructure.Models;
+using Flurl.Http;
 
 // ReSharper disable CheckNamespace
 
@@ -10,5 +14,41 @@ namespace BuildMaster.Net
             .AppendPathSegment(path)
             .SetQueryParams(queryParamValues)
             .AllowAnyHttpStatus();
+
+        public async Task<IEnumerable<Server>> ListServersAsync()
+        {
+            var response = await GetInfrastructureApiClient("servers/list")
+                .PostAsync(new StringContent(""))
+                .ReceiveJson<IEnumerable<Server>>();
+
+            return response; //TODO: inline
+        }
+
+        public async Task<Server> CreateServerAsync(Server server)
+        {
+            var response = await GetInfrastructureApiClient($"servers/create/{server?.Name}")
+                .PostJsonAsync(server)
+                .ReceiveJson<Server>();
+
+            return response; //TODO: inline
+        }
+
+        public async Task<Server> UpdateServerAsync(Server server)
+        {
+            var response = await GetInfrastructureApiClient($"servers/update/{server?.Name}")
+                .PostJsonAsync(server)
+                .ReceiveJson<Server>();
+
+            return response; //TODO: inline
+        }
+
+        public async Task<Server> DeleteServerAsync(Server server)
+        {
+            var response = await GetInfrastructureApiClient($"servers/delete/{server?.Name}")
+                .PostJsonAsync(server)
+                .ReceiveJson<Server>();
+
+            return response; //TODO: inline
+        }
     }
 }
