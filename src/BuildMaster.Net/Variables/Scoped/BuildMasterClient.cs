cb89ab8bc3ae3926042e548e673f9b5763b43026
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-02 18:26:40 +0200

    add variables/scoped

---
+++ src/BuildMaster.Net/Variables/Scoped/BuildMasterClient.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BuildMaster.Net.Common.Models;
+using Flurl.Http;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net
+{
+    public partial class BuildMasterClient
+    {
+        public async Task<IEnumerable<ScopedVariable>> GetAllScopedConfigurationVariables()
+        {
+            var response = await GetVariablesApiClient("scoped/all")
+                .GetJsonAsync<IEnumerable<ScopedVariable>>();
+
+            return response; //TODO: inline
+        }
+
+        public async Task<bool> SetAllScopedConfigurationVariables(IEnumerable<ScopedVariable> variables)
+        {
+            var response = await GetVariablesApiClient("scoped/all")
+                .PutJsonAsync(variables);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<ScopedVariable> GetSingleScopedConfigurationVariable(string variableName)
+        {
+            var response = await GetVariablesApiClient($"scoped/single/{variableName}")
+                .GetJsonAsync<ScopedVariable>();
+
+            return response; //TODO: inline
+        }
+
+        public async Task<bool> SetSingleScopedConfigurationVariable(ScopedVariable variable)
+        {
+            var response = await GetVariablesApiClient($"scoped/single/{variable?.Name}")
+                .PutJsonAsync(variable);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteSingleScopedConfigurationVariable(string variableName)
+        {
+            var response = await GetVariablesApiClient($"scoped/single/{variableName}")
+                .DeleteAsync();
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
