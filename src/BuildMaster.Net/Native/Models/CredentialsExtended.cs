3a0a7df86fad371381fe50430a86bca57ef870e1
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-03 08:38:31 +0200

    implement CredentialsExtended

--- src/BuildMaster.Net/Native/Models/CredentialsExtended.cs
+++ src/BuildMaster.Net/Native/Models/CredentialsExtended.cs
@@ -1,6 +1,15 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class CredentialsExtended
     {
+        public int Credential_Id { get; set; }
+        public int Environment_Id { get; set; }
+        public string Credential_Name { get; set; }
+        public string CredentialType_Name { get; set; }
+        public string Configuration_Xml { get; set; }
+        public bool AllowFunctionAccess_Indicator { get; set; }
+        public string Environment_Name { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/Credentials/BuildMasterClientShould.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task Credentials_GetCredentialsAsync()
+        {
+            var results = await _client.Credentials_GetCredentialsAsync().ConfigureAwait(false);
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
