09969c3b42a26bef9dadbba3b492b3ae65bb9c83
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-04 18:36:57 +0200

    add Executions_* (empty models)

---
+++ src/BuildMaster.Net/Native/Executions/BuildMasterClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using BuildMaster.Net.Common;
+using BuildMaster.Net.Native.Models;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net
+{
+    public partial class BuildMasterClient
+    {
+        /// <summary>
+        /// Gets a manual execution
+        /// </summary>
+        public async Task<ManualExecutionsExtended> Executions_GetManualExecutionAsync(int execution_Id)
+        {
+            var queryParamValues = QueryParamValues.From(
+                new NamedValue(nameof(execution_Id).Capitalize(), execution_Id)
+            );
+
+            return await ExecuteNativeApiMethodAsync<ManualExecutionsExtended>(nameof(Executions_GetManualExecutionAsync).WithoutAsyncSuffix(), queryParamValues).ConfigureAwait(false);
+        }
+    }
+}
