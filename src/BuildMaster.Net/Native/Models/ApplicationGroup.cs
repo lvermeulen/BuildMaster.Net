39e2c1c1018a3906d3610d2c888779180ba0ef87
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-05 11:42:22 +0200

    add application group tests

--- src/BuildMaster.Net/Native/Models/ApplicationGroup.cs
+++ src/BuildMaster.Net/Native/Models/ApplicationGroup.cs
@@ -1,6 +1,18 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class ApplicationGroup
     {
+        public int ApplicationGroup_Id { get; set; }
+        public string ApplicationGroup_Name { get; set; }
+        public string ApplicationGroup_Description { get; set; }
+        public string CreatedBy_User_Name { get; set; }
+        public DateTime CreatedOn_Date { get; set; }
+        public string ModifiedBy_User_Name { get; set; }
+        public DateTime? ModifiedOn_Date { get; set; }
+        public int? Parent_ApplicationGroup_Id { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/ApplicationGroups/BuildMasterClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task ApplicationGroups_GetApplicationGroupsAsync()
+        {
+            var results = await _client.ApplicationGroups_GetApplicationGroupsAsync().ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
