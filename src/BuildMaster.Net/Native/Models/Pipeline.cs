30cb55d9dcc73ce3057d5c72be8c8729ec72de71
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-05 12:17:27 +0200

    add pipeline tests

--- src/BuildMaster.Net/Native/Models/Pipeline.cs
+++ src/BuildMaster.Net/Native/Models/Pipeline.cs
@@ -1,6 +1,19 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class Pipeline
     {
+        public int Pipeline_Id { get; set; }
+        public string Pipeline_Name { get; set; }
+        public string Pipeline_Configuration { get; set; }
+        public bool Active_Indicator { get; set; }
+        public int Application_Id { get; set; }
+        public DateTime? ModifiedOn_Date { get; set; }
+        public string ModifiedBy_User_Name { get; set; }
+        public int? MigratedFrom_Workflow_Id { get; set; }
+        public string Pipeline_Color { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/Pipelines/BuildMasterClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task Pipelines_GetPipelinesAsync()
+        {
+            var results = await _client.Pipelines_GetPipelinesAsync().ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
