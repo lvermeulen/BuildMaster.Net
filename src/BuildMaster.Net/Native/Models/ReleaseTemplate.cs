824ba0fb64f11f09d56e0035c1e283813d692282
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-05 10:56:06 +0200

    add release template tests

--- src/BuildMaster.Net/Native/Models/ReleaseTemplate.cs
+++ src/BuildMaster.Net/Native/Models/ReleaseTemplate.cs
@@ -1,6 +1,12 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class ReleaseTemplate
     {
+        public int ReleaseTemplate_Id { get; set; }
+        public int Application_Id { get; set; }
+        public string ReleaseTemplate_Name { get; set; }
+        public string ReleaseTemplate_Configuration { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/ReleaseTemplates/BuildMasterClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task ReleaseTemplates_GetTemplatesAsync()
+        {
+            var results = await _client.ReleaseTemplates_GetTemplatesAsync(1).ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
