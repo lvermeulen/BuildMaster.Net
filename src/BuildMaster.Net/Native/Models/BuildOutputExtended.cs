c16b9858957f41da06fbf4018d3ef01d4ef84ed6
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-07 15:57:25 +0200

    add build outputs tests

--- src/BuildMaster.Net/Native/Models/BuildOutputExtended.cs
+++ src/BuildMaster.Net/Native/Models/BuildOutputExtended.cs
@@ -1,6 +1,26 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class BuildOutputExtended
     {
+        public int Execution_Id { get; set; }
+        public int Output_Sequence { get; set; }
+        public string Output_Name { get; set; }
+        public byte[] Output_Bytes { get; set; }
+        public string OutputType_Code { get; set; }
+        public int Application_Id { get; set; }
+        public int Release_Id { get; set; }
+        public string Release_Number { get; set; }
+        public int Build_Id { get; set; }
+        public string Build_Number { get; set; }
+        public int Promotion_Id { get; set; }
+        public string PipelineStage_Name { get; set; }
+        public int? Environment_Id { get; set; }
+        public string Environment_Name { get; set; }
+        public DateTime ExecutionStart_Date { get; set; }
+        public string ExecutionStatus_Name { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/BuildOutputs/BuildMasterClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task BuildOutputs_GetOutputsAsync()
+        {
+            var results = await _client.BuildOutputs_GetOutputsAsync(1).ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+
+    }
+}
