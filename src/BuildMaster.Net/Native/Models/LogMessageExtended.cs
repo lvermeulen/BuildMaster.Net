44cbd5bf12d200caac8e68af97d4e5c155e8f7a7
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-08 23:11:32 +0200

    add log messages tests

--- src/BuildMaster.Net/Native/Models/LogMessageExtended.cs
+++ src/BuildMaster.Net/Native/Models/LogMessageExtended.cs
@@ -1,6 +1,29 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class LogMessageExtended
     {
+        public int LogMessage_Id { get; set; }
+        public DateTime Logged_Date { get; set; }
+        public string Message_Text { get; set; }
+        public string Category_Name { get; set; }
+        public int Message_Level { get; set; }
+        public byte[] Details_Bytes { get; set; }
+        public int? Server_Id { get; set; }
+        public string Server_Name { get; set; }
+        public int? Application_Id { get; set; }
+        public string Application_Name { get; set; }
+        public int? Schedule_Id { get; set; }
+        public string Schedule_Name { get; set; }
+        public int? Requirement_Id { get; set; }
+        public string Requirement_Description { get; set; }
+        public int? Requirement_Workflow_Id { get; set; }
+        public int? Provider_Id { get; set; }
+        public string Provider_Name { get; set; }
+        public int? EventListener_Id { get; set; }
+        public bool ShowNotification_Indicator { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/LogMessages/BuildMasterClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task LogMessages_GetMessagesAsync()
+        {
+            var results = await _client.LogMessages_GetMessagesAsync().ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
