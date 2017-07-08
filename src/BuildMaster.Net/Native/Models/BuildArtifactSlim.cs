dbb93c27d0ea7297a1bf7f596600b13d532b5aef
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-08 13:08:45 +0200

    add artifacts tests

--- src/BuildMaster.Net/Native/Models/BuildArtifactSlim.cs
+++ src/BuildMaster.Net/Native/Models/BuildArtifactSlim.cs
@@ -1,6 +1,21 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class BuildArtifactSlim
     {
+        public int Application_Id { get; set; }
+        public int Release_Id { get; set; }
+        public string Release_Number { get; set; }
+        public int Build_Id { get; set; }
+        public string Build_Number { get; set; }
+        public int? Deployable_Id { get; set; }
+        public string Artifact_Name { get; set; }
+        public int? Execution_Id { get; set; }
+        public DateTime Created_Date { get; set; }
+        public string BuildStatus_Name { get; set; }
+        public string Artifact_Path { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/Artifacts/BuildMasterClientShould.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task Artifacts_GetArtifactsAsync()
+        {
+            var results = await _client.Artifacts_GetArtifactsAsync(1, null, null, 0, 1000).ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+
+        [Fact]
+        public async Task Artifacts_GetArtifactsByBuildStatusAsync()
+        {
+            var results = await _client.Artifacts_GetArtifactsByBuildStatusAsync(1, null, null).ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
