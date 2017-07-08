b5c803d198060ee96d972da50430a4e83ab69ed9
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-08 23:43:24 +0200

    add user configuration tests

--- src/BuildMaster.Net/Native/Models/UserConfiguration.cs
+++ src/BuildMaster.Net/Native/Models/UserConfiguration.cs
@@ -1,6 +1,11 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class UserConfiguration
     {
+        public string User_Name { get; set; }
+        public string Key_Name { get; set; }
+        public string Value_Text { get; set; }
     }
 }
--- test/BuildMaster.Net.Tests/Native/UserConfiguration/BuildMasterClientShould.cs
+++ test/BuildMaster.Net.Tests/Native/UserConfiguration/BuildMasterClientShould.cs
@@ -7,5 +7,13 @@ namespace BuildMaster.Net.Tests
 {
     public partial class BuildMasterClientShould
     {
+        [Fact]
+        public async Task UserConfiguration_GetUserConfigurationAsync()
+        {
+            var results = await _client.UserConfiguration_GetUserConfigurationAsync("Admin").ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
     }
 }
