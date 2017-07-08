c9b4b20c863e25f7055af7b8222b44e00225a6ca
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-08 23:40:28 +0200

    add UserConfiguration_* (empty models)

--- src/BuildMaster.Net/Native/UserConfiguration/BuildMasterClient.cs
+++ src/BuildMaster.Net/Native/UserConfiguration/BuildMasterClient.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using BuildMaster.Net.Common;
+using BuildMaster.Net.Native.Models;
 
 // ReSharper disable InconsistentNaming
 // ReSharper disable CheckNamespace
@@ -8,20 +10,51 @@ namespace BuildMaster.Net
 {
     public partial class BuildMasterClient
     {
-        //UserConfiguration_GetUserConfiguration
-        //Gets the a complete list of user configuration settings
-        //User_Name(String)  DataTable(UserConfiguration)
+        /// <summary>
+        /// Gets the complete list of user configuration settings
+        /// </summary>
+        public async Task<IEnumerable<UserConfiguration>> UserConfiguration_GetUserConfigurationAsync(string user_Name)
+        {
+            var queryParamValues = QueryParamValues.From(
+                new NamedValue(nameof(user_Name).Capitalize(), user_Name)
+            );
 
-        //UserConfiguration_GetValue
-        //Gets the specified user configuration value
-        //User_Name(String)
-        //Key_Name(String)
-        //Value_Text(String) Value_Text
-        
-        //UserConfiguration_SetValue
-        //Sets a user configuration value
-        //User_Name(String)
-        //Key_Name(String)
-        //Value_Text(String) none
+            return await ExecuteNativeApiMethodAsync<IEnumerable<UserConfiguration>>(nameof(UserConfiguration_GetUserConfigurationAsync).WithoutAsyncSuffix(), queryParamValues).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Gets the specified user configuration value
+        /// </summary>
+        /// <returns>Value_Text</returns>
+        public async Task<string> UserConfiguration_GetValueAsync(
+            string user_Name,
+            string key_Name,
+            string value_Text)
+        {
+            var queryParamValues = QueryParamValues.From(
+                new NamedValue(nameof(user_Name).Capitalize(), user_Name),
+                new NamedValue(nameof(key_Name).Capitalize(), key_Name),
+                new NamedValue(nameof(value_Text).Capitalize(), value_Text)
+            );
+
+            return await ExecuteNativeApiMethodAsync<string>(nameof(UserConfiguration_GetValueAsync).WithoutAsyncSuffix(), queryParamValues).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Sets a user configuration value
+        /// </summary>
+        public async Task<bool> UserConfiguration_SetValueAsync(
+            string user_Name,
+            string key_Name,
+            string value_Text)
+        {
+            var queryParamValues = QueryParamValues.From(
+                new NamedValue(nameof(user_Name).Capitalize(), user_Name),
+                new NamedValue(nameof(key_Name).Capitalize(), key_Name),
+                new NamedValue(nameof(value_Text).Capitalize(), value_Text)
+            );
+
+            return await ExecuteNativeApiMethodAsync(nameof(UserConfiguration_SetValueAsync).WithoutAsyncSuffix(), queryParamValues).ConfigureAwait(false);
+        }
     }
 }
