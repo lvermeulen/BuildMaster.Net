f80a8a7ab9dc0a2764d2daaf2005bf838b8c7805
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-09 10:12:09 +0200

    add script assets tests

--- src/BuildMaster.Net/Native/Models/ScriptAsset.cs
+++ src/BuildMaster.Net/Native/Models/ScriptAsset.cs
@@ -1,6 +1,16 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class ScriptAsset
     {
+        public int ScriptAsset_Id { get; set; }
+        public string ScriptAsset_Name { get; set; }
+        public int? Application_Id { get; set; }
+        public byte[] Script_Text { get; set; }
+        public string ModifiedBy_User_Name { get; set; }
+        public DateTime ModifiedOn_Date { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/ScriptAssets/BuildMasterClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task ScriptAssets_GetScriptsAsync()
+        {
+            var results = await _client.ScriptAssets_GetScriptsAsync(1).ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
