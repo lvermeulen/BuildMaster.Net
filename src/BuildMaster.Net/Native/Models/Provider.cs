40d3da957e0d11a787d88d4db0bde60506d953e9
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-09 18:51:39 +0200

    add providers tests

--- src/BuildMaster.Net/Native/Models/Provider.cs
+++ src/BuildMaster.Net/Native/Models/Provider.cs
@@ -1,6 +1,21 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class Provider
     {
+        public int Provider_Id { get; set; }
+        public string ProviderType_Code { get; set; }
+        public string Provider_Name { get; set; }
+        public string Provider_Description { get; set; }
+        public string Provider_Configuration { get; set; }
+        public int? ExecuteOn_Server_Id { get; set; }
+        public string CreatedBy_User_Name { get; set; }
+        public DateTime? CreatedOn_Date { get; set; }
+        public string ModifiedBy_User_Name { get; set; }
+        public DateTime? ModifiedOn_Date { get; set; }
+        public bool Internal_Indicator { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/Providers/BuildMasterClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task Providers_GetProvidersAsync()
+        {
+            var results = await _client.Providers_GetProvidersAsync().ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
