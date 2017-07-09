dec53f8b742270d0691044304764cdaa5b966d18
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-09 09:57:26 +0200

    add configuration tests

--- src/BuildMaster.Net/Native/Models/Configuration.cs
+++ src/BuildMaster.Net/Native/Models/Configuration.cs
@@ -1,6 +1,12 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class Configuration
     {
+        public string Key_Name { get; set; }
+        public string Value_Text { get; set; }
+        public string Legacy_Scope_Name { get; set; }
+        public string Legacy_Key_Name { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/Configuration/BuildMasterClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task Configuration_GetConfigurationAsync()
+        {
+            var results = await _client.Configuration_GetConfigurationAsync().ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
