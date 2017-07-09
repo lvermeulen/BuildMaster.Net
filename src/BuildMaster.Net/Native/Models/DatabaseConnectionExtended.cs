54e8dad46c71634550603dd1cf7123b541bad630
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-09 18:14:28 +0200

    add database connections tests

--- src/BuildMaster.Net/Native/Models/DatabaseConnectionExtended.cs
+++ src/BuildMaster.Net/Native/Models/DatabaseConnectionExtended.cs
@@ -1,11 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+// ReSharper disable InconsistentNaming
 
 namespace BuildMaster.Net.Native.Models
 {
     public class DatabaseConnectionExtended
     {
+        public int DatabaseConnection_Id { get; set; }
+        public string DatabaseConnection_Name { get; set; }
+        public string DatabaseConnection_Description { get; set; }
+        public string DatabaseConnection_Configuration { get; set; }
+        public int? ExecuteOn_Server_Id { get; set; }
+        public string ExecuteOn_Server_Name { get; set; }
+        public string ModifiedBy_User_Name { get; set; }
+        public DateTime ModifiedOn_Date { get; set; }
+        public int Application_Id { get; set; }
+        public string Application_Name { get; set; }
+        public int Environment_Id { get; set; }
+        public string Environment_Name { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/DatabaseConnections/BuildMasterClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task DatabaseConnections_GetConnectionsAsync()
+        {
+            var results = await _client.DatabaseConnections_GetConnectionsAsync().ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
