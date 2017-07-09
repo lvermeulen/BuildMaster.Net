7b2c4d59b54fb9807542600a65fcc1e922ffe7ca
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-09 09:40:14 +0200

    add change control performances tests

--- src/BuildMaster.Net/Native/Models/ChangeControlPerformanceReleaseSummary.cs
+++ src/BuildMaster.Net/Native/Models/ChangeControlPerformanceReleaseSummary.cs
@@ -1,6 +1,18 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class ChangeControlPerformanceReleaseSummary
     {
+        public int ChangeControl_Id { get; set; }
+        public string Environment_Name { get; set; }
+        public int Environment_Id { get; set; }
+        public int? ChangeControlPerformance_Id { get; set; }
+        public string Notes_Text { get; set; }
+        public string PerformedBy_User_Name { get; set; }
+        public DateTime? Performed_Date { get; set; }
+        public bool ChangeControlPerformed_Indicator { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/ChangeControlPerformances/BuildMasterClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task ChangeControlPerformances_GetReleaseSummaryAsync()
+        {
+            var results = await _client.ChangeControlPerformances_GetReleaseSummaryAsync(1).ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
