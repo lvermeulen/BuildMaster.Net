a51602809e675b42992b564b7012af91f67d7418
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-09 18:28:42 +0200

    add extension configuration tests

--- src/BuildMaster.Net/Native/Models/ExtensionConfiguration.cs
+++ src/BuildMaster.Net/Native/Models/ExtensionConfiguration.cs
@@ -1,6 +1,19 @@
-﻿namespace BuildMaster.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace BuildMaster.Net.Native.Models
 {
     public class ExtensionConfiguration
     {
+        public int ExtensionConfiguration_Id { get; set; }
+        public string QualifiedType_Name { get; set; }
+        public string Profile_Name { get; set; }
+        public string Extension_Configuration { get; set; }
+        public string CreatedBy_User_Name { get; set; }
+        public DateTime CreatedOn_Date { get; set; }
+        public string ModifiedBy_User_Name { get; set; }
+        public DateTime ModifiedOn_Date { get; set; }
+        public bool Default_Indicator { get; set; }
     }
 }
---
+++ test/BuildMaster.Net.Tests/Native/ExtensionConfiguration/BuildMasterClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace BuildMaster.Net.Tests
+{
+    public partial class BuildMasterClientShould
+    {
+        [Fact]
+        public async Task ExtensionConfiguration_GetConfigurationsAsync()
+        {
+            var results = await _client.ExtensionConfiguration_GetConfigurationsAsync().ConfigureAwait(false);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
