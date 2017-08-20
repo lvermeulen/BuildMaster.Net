243c2ca2e1e4fca66f6970a3e7df061c24e31b64
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-08-20 19:08:56 +0200

    update build.ps1

--- build/build.ps1
+++ build/build.ps1
@@ -11,8 +11,7 @@ foreach ($src in ls $PSScriptRoot\..\src/*) {
 	echo "build: Building & packaging project in $src"
 
     & dotnet build -c Release --version-suffix=$BuildVersionNumber
-	Write-Host "dotnet pack -c Release --include-symbols -o ..\..\artifacts --version-suffix=$BuildVersionNumber --no-build"
-    #& dotnet pack -c Release --include-symbols -o ..\..\artifacts --version-suffix=$TagVersionNumber --no-build
+    & dotnet pack -c Release --include-symbols -o ..\..\artifacts --version-suffix=$BuildVersionNumber --no-build
     if($LASTEXITCODE -ne 0) { exit 1 }    
 
     Pop-Location
